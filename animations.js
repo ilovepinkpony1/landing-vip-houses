30a5cf67d42798e278973a003e41def41de56645
Vitalii <ilovepinkpony1@gmail.com>
2019-06-07 08:02:08 +0300

    created simple landing

---
+++ animations.js
@@ -0,0 +1,23 @@
+const tl = new TimelineMax();
+
+tl.fromTo('.logo-header', 1, {
+    x: -70,
+    opacity: 0
+  }, {
+    x: 0,
+    opacity: 1
+  })
+  .staggerFromTo('.navbar-content__a', 0.5, {
+    opacity: 0,
+    y: 10
+  }, {
+    opacity: 1,
+    y: 0
+  }, 0.25)
+  .fromTo('.header-large__caption', 1.5, {
+    y: -20,
+    opacity: 0
+  }, {
+    y: 0,
+    opacity: 1
+  })
